8107a342302b9c1a5f8ea811b1260cace67460cf
MimoriAyano <anhtung0205a@gmail.com>
2024-06-13 12:51:04 +0700

    Submit CS_Unity

---
+++ Assets/EnemyGFX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyGFX : MonoBehaviour
+{
+    public AIPath aiPath;
+    // Update is called once per frame
+    void Update() {
+        //thay đổi hướng của ENEMY theo tốc độ
+
+        if (aiPath.desiredVelocity.x >= 0.01f) {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+
+        } else if (aiPath.desiredVelocity.x < 0.01f) {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+}
